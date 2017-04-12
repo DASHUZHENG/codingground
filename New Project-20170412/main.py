7328d52da0670a4ddcc2aefadb767a1c5893bded
DASHUZHENG <zyxismmm@163.com>
2017-04-12 16:55:34 +0800

    Created by Coding Ground Client

---
+++ New Project-20170412/main.py
@@ -0,0 +1,18 @@
+# Hello World program in Python
+    
+print "Hello World!\n"
+
+a="H"
+b="T"
+
+t={"(H,T)":1}
+
+print a, b, t
+
+c="(%s,%s)" % (a,b)
+
+print t[c],"What!"
+
+for comp in t:
+    print c
+    print comp
